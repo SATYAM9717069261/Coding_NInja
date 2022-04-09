f1ecb382b8f376ae28937510aa3356aec1870d3d
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2022-04-10 03:54:21 +0530

    Use Binary Search

--- Momos_Market.cpp
+++ Momos_Market.cpp
@@ -1,29 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-void solution(int nshop,int* shop, int nday, int* day){
-	for(int i=0;i<nday;i++){
-		int momo=0;
-		for(int j=0;j<nshop;j++){
-			if(shop[j]<=day[i]){
-				day[i]-=shop[j];
-				momo++;
-			}else
-				break;
-		}
-		cout<<"momo=> "<<momo<<" remain money=> "<<day[i];
-	}	
+void solution(long nshop,long* shop, long nday, long* day){
+        for(long i=1;i<nshop;i++)
+                shop[i]=shop[i-1]+shop[i];
+        for(long i=0;i<nday;i++){
+                long start=0;
+                long end=nshop-1;
+                long mid=(end+start)/2;
+                long momo=shop[end]<day[i]?end:0;
+                while(mid>start && mid < end && shop[0] < day[i] ){
+                        if(day[i]>=shop[mid]){
+                                momo=mid;
+                                start=mid;
+                        }else
+                                end=mid;
+                        mid=(end+start)/2;
+                }
+            if(shop[0]<day[i])
+                cout<<momo+1<<" "<<day[i]-shop[momo]<<endl;
+            else
+                cout<<0<<" "<<day[i]<<endl;
+        }
 }
+
 
 int main(){
-	int nshop;cin>>nshop;
-	int shop[nshop];
-	for(int i=0;i<nshop;i++)
-		cin>>shop[i];
-	int nday;cin>>nday;
-	int day[nday];
-	for(int i=0;i<nday;i++)
-		cin>>day[i];
-	solution(nshop,shop,nday,day);
-	return 0;
-}
+        long nshop;cin>>nshop;
+        long shop[nshop];
+        for(int i=0;i<nshop;i++)
+                cin>>shop[i];
+        long nday;cin>>nday;
+        long day[nday];
+        for(long i=0;i<nday;i++)
+                cin>>day[i];
+        solution(nshop,shop,nday,day);
+        return 0;
+} 
