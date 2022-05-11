77da621feacb510dabfcc0ed319e4012b1748439
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2022-05-11 17:23:43 +0530

    Place The Word Solved

--- crossword.cpp
+++ crossword.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define LIMIT 10
-#define Words 4
 void print(char arr[][LIMIT]){
 	for(int i=0;i<LIMIT;i++){
 		for(int j=0;j<LIMIT;j++)
@@ -10,23 +9,19 @@ void print(char arr[][LIMIT]){
 	}
 }
 void placedLettersPrint(vector<pair<int,int>> data){
-	cout<<"Record= >";
 	for(pair<int,int> i : data)
 		cout<<i.first<<" "<<i.second<<endl;
 	cout<<endl;
 }
 bool verticalPlaceWords(string word, int x,int y , char board[][LIMIT],vector< pair<int,int>>& placedLetters){
-	//cout<<"Try to fit => "<<word;
 	int wordStart = 0; 
 	for(int i=x;i<LIMIT && wordStart < word.size();i++){
 		if(board[i][y] == '-' || board[i][y] == word[wordStart]){
 			if (board[i][y] == '-') 
 				placedLetters.push_back(make_pair(i,y));
 			board[i][y] = word[wordStart++];
-		}else{
-			//placedLettersPrint(placedLetters);
+		}else
 			return false;
-		}
 	}
 	return wordStart == word.size() ? true:false; 
 }
@@ -37,61 +32,39 @@ bool horizontaalPlaceWords(string word, int x,int y, char board[][LIMIT],vector<
 			if( board[x][j] == '-')
 				placedLetters.push_back(make_pair(x,j));
 			board[x][j] = word[wordStart++];
-		}else{
+		}else
 			return false;
-		}
+		
 	}
 	return wordStart == word.size() ? true: false; 
 }
 void reverse(char board[][LIMIT],vector<pair<int,int>> places){
-	for(pair<int,int> i: places){
+	for(pair<int,int> i: places) 
 		board[i.first][i.second] = '-';
-	}
-	 //print(board);
 }
-bool solution(char board[][LIMIT],string letters[],int lettercount=Words){
-	//cout<<"Place => "<< letters[0]<<"letter Count => "<<lettercount<<endl;
-	//Store where we put letters
-	if(lettercount == 0){
-		//cout<<"Finnal => "<<endl;
-		//print(board);
+bool solution(char board[][LIMIT],vector<string> letters,int start=0){
+	if(start == letters.size()){
 		return true;
 	}
 	for(int i=0;i<LIMIT;i++){
 		for(int j=0;j<LIMIT;j++){
-			if(board[i][j] == '-' || board[i][j] == letters[0][0]){
+			if(board[i][j] == '-' || board[i][j] == letters[start][0]){
 				vector< pair<int,int> > h_placedLetters;
-	//			cout<<"Blank Space = > "<< i << j<<endl;
-				if(verticalPlaceWords(letters[0],i,j,board,h_placedLetters)){	
-	//				print(board);
-					if(solution(board,letters+1,lettercount-1 )){ //false -> revert last changes and continue
+				if(verticalPlaceWords(letters[start],i,j,board,h_placedLetters)){
+					if(solution(board,letters,start+1 )) 
 						return true;
-					}
-					else{
-	//					cout<<"Not Fit=> "<< letters+1 << "Revert => "<< letters[0];
+					else 
 						reverse(board,h_placedLetters);
-					}
-				}else{
-	//				placedLettersPrint(h_placedLetters);
+				}else
 					reverse(board,h_placedLetters);
-	//				cout<<"Revert it => "<<endl;
-	//				print(board);
-				}
 				vector< pair<int,int> > v_placedLetters;
-				if(horizontaalPlaceWords(letters[0],i,j,board,v_placedLetters)){
-	//				print(board);
-				       	if(solution(board,letters+1,lettercount-1)){
+				if(horizontaalPlaceWords(letters[start],i,j,board,v_placedLetters)){
+					if(solution(board,letters,start+1))
 						return true;
-					}else{
-	//					cout<<"Not Fit=> "<< letters+1 << "Revert => "<< letters[0];
-						reverse(board,h_placedLetters);
-					}
-				}else{
-	//				placedLettersPrint(v_placedLetters);
+					else 
+						reverse(board,v_placedLetters);
+				}else
 					reverse(board,v_placedLetters);
-	//				cout<<"Revert it => "<<endl;
-	//				print(board);
-				}
 			}
 		}
 	}
@@ -99,13 +72,27 @@ bool solution(char board[][LIMIT],string letters[],int lettercount=Words){
 }
 int main(){
 	char board[LIMIT][LIMIT];
-	string letters[Words]={"CALIFORNIA","NIGERIA","CANADA","TELAVIV"};
+	string inp;
 	for(int i=0;i<LIMIT;i++){
 		for(int j=0;j<LIMIT;j++){
 			cin>>board[i][j];
 		}
 	}
-       	if( solution(board,letters) )
+	cin>>inp;
+	vector<string> stringInp;
+	string currentstring="";
+	for(char i : inp){
+		if(i==';'){
+			stringInp.push_back(currentstring);
+			currentstring = "";
+		}
+		else
+			currentstring+=i;
+	}
+	stringInp.push_back(currentstring);
+//	for(int i = 0;i<stringInp.size();i++)
+//		cout<<stringInp[i]<<endl;
+       	if( solution(board,stringInp) )
 		print(board);
 	else
 		cout<<"Not Posible ";
