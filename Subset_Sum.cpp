ee4fe3b702f11b2c5f9c9f5dfb2cebcb501d544e
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2022-05-14 21:12:21 +0530

    SubSet value find Solve

--- Subset_Sum.cpp
+++ Subset_Sum.cpp
@@ -1,33 +1,24 @@
-/**
-IN Coding ninjas try this test case
-Test Case +> 1
-	1 0
-	0
-Expected Ouput => 2
-Actual Output => 1
-**/
 #include<bits/stdc++.h>
 using namespace std;
 int solution(int arr[],int num,int arr_size,int sum=0){
-	if(sum > num)
-		return 0;
-	if(sum == num )
-		return 1;
-	int left=0;
-	int right =0;
-	if(arr_size!=0){
-		left = solution(arr+1,num,arr_size-1,sum+arr[0]);
-		right = solution(arr+1,num,arr_size-1,sum);
-	}else
-		return 0;
+	if (arr_size == 0) {
+        if (sum == num) {
+	        return 1;
+        } else return 0;
+	}
+    int left = solution(arr+1,num,arr_size-1,sum+arr[0]);
+	int right = solution(arr+1,num,arr_size-1,sum);
 	return left+right;
 }
 int main(){
-	int arr_size;cin>>arr_size;
-	int num;cin>>num;
-	int arr[arr_size];
-	for(int i=0;i<arr_size;i++)
-		cin>>arr[i];
-	cout<<solution(arr,num,arr_size);
+    int test;cin>>test;
+    while(test--){
+        int arr_size;cin>>arr_size;
+        int num;cin>>num;
+        int arr[arr_size];
+        for(int i=0;i<arr_size;i++)
+            cin>>arr[i];
+        cout<<solution(arr,num,arr_size)<<endl;
+    }
 	return 0;
 }
