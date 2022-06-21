402a7ef370af37404f1bd7823da80814715a2a8c
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2022-06-21 23:26:08 +0530

    Trade Profit Not Complete

---
+++ Trade_Profit1.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solution(int* arr, int start,int end,bool status , int trans,int*** result){
+  if(start > end || trans < 0)
+    return 0;
+  if(result[trans][status][start]!=-1)
+    return result[trans][status][start];
+  int skip = solution(arr+1,start+1,end,status,trans,result);
+  int profit= 0;
+  if(status == 0){
+    if(trans > 0){
+      profit = solution(arr+1,start+1,end,1,trans,result) - arr[0];
+ 	  result[trans][status][start]=max(skip,profit);     
+      return max(profit,skip);
+    }
+  }else{
+    profit = solution(arr+1,start+1,end,0,trans-1,result)+arr[0];
+  	result[trans][status][start]=max(skip,profit);
+    return max(profit,skip);
+  }
+  result[trans][status][start]=max(skip,profit);
+  return result[trans][status][start];
+}
+
+
+
+int main(){
+      int maxtrade;cin>>maxtrade;
+      int size;cin>>size;
+      int arr[size];
+      for(int i=0;i<size;i++)
+        cin>>arr[i];
+      int*** result = new int**[maxtrade+1];
+      for(int j=0;j<=maxtrade;j++){  
+          result[j]=new int*[2];
+          result[j][0] = new int[size];
+          result[j][1] = new int[size];
+          for(int i=0;i<=size;i++){
+            result[j][0][i]=-1;
+            result[j][1][i]=-1;
+          }
+      }
+      cout<<solution(arr,0,size-1,0,maxtrade,result)<<endl;
+      for(int j=0;j<=maxtrade;j++){
+        delete result[j][0];
+        delete result[j][1];
+        delete result[j];
+      }
+      delete result;
+  return 0;
+}
+
