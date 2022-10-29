e01e5c51f3bc7d24b491acf1ed486b887219ce52
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2022-10-30 01:51:04 +0530

    find All palindrom

--- palindrom.cpp
+++ palindrom.cpp
@@ -1,43 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-void longestPalindrom(int* arr, string str, int strlen){
+int longestPalindrom(string str, int strlen){
   int start = 0;
   int end = strlen;
+  int max = 0;
   for(int i=0;i<end;i++){
     int left = i;
     int right = i;
     int count = 0;
-    int max = 0;
     while( left >= start && right <= strlen && str[left] == str[right]){
-        if(left == right)
           count++;
-        else
-          count+=2;
         left--; right++; 
-    }
-    if(max < count) 
-      max=count;
+    } 
+    max += count;
     left = i;
     right = i+1;
     count = 0;
     while(left >= start && right <= strlen && str[left] == str[right]){
-        if(left == right)
           count++;
-        else
-          count+=2;
         left--; right++;
     }
-    if(max < count)
-      max=count;
-    arr[i] = max;
+    max += count;
   }
+  return max;
 }
 int main(){
-  string str;cin>>str;
-  int strLen = str.length();
-  int* arr = new int[strLen];
-  longestPalindrom(arr,str,strLen);
-  for(int i = 0;i<strLen;i++)
-    cout<<" = >"<<arr[i]<<endl;
+  int test;cin>>test;
+  while(test--){
+    string str;cin>>str;
+    int strLen = str.length();
+    cout<<longestPalindrom(str,strLen)<<endl;
+  }
   return 0;
 } 
